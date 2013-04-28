761d2fb19d7ecb4d7261edaf04872f9265a57283
NatalieBlack <natalievictoriablack@gmail.com>
2013-04-28 00:42:32 -0400

    add music

--- teganandsara.py
+++ teganandsara.py
@@ -6,12 +6,13 @@ class GameObject(object):
         self.hor_speed = hspeed
         self.ver_speed = vspeed
         self.image = image
-        self.pos = image.get_rect().move(0, height)
+        self.rect = image.get_rect().move(0, height)
+        
     def set_speed(self, x, y):
         self.hor_speed = x
         self.ver_speed = y
     def move(self):
-        self.pos = self.pos.move(self.hor_speed, self.ver_speed)
+        self.rect = self.rect.move(self.hor_speed, self.ver_speed)
 
 class BackgroundSprite(pygame.sprite.Sprite):
     def __init__(self, img, xposition):
@@ -19,7 +20,7 @@ class BackgroundSprite(pygame.sprite.Sprite):
 
         self.image = pygame.image.load(img)
 
-        self.rect = (xposition,0)
+        self.rect = self.image.get_rect().move(xposition, 0)
 
 screen = pygame.display.set_mode((896, 480))
 player = pygame.image.load('guitar_small.png')
@@ -27,6 +28,13 @@ tegan = BackgroundSprite('tegan.jpg', 0)
 sara = BackgroundSprite('sara.jpg', 396)
 backgrounds = pygame.sprite.Group(tegan, sara)
 backgrounds.draw(screen)
+pygame.mixer.init()
+sarasound = pygame.mixer.Sound('Tegan and Sara - Floorplan.ogg')
+tegansound = pygame.mixer.Sound('Tegan and Sara - Nineteen.ogg')
+sarachannel = sarasound.play()
+sarachannel.pause()
+teganchannel = tegansound.play()
+teganchannel.pause()
 o = GameObject(player, 40, 0, 0)
 while 1:
     for event in pygame.event.get():
@@ -45,6 +53,16 @@ while 1:
             o.set_speed(0,0)
     backgrounds.draw(screen)
     o.move()
-    screen.blit(o.image, o.pos)
+    screen.blit(o.image, o.rect)
+    overlapping = pygame.sprite.spritecollide(o, backgrounds, False)
+    if sara in overlapping:
+        sarachannel.unpause()
+        teganchannel.pause()
+    elif tegan in overlapping:
+        teganchannel.unpause()
+        sarachannel.pause()
+    else:
+        sarachannel.pause()
+        teganchannel.pause()
     pygame.display.update()
     pygame.time.delay(100)
